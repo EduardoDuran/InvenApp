fde5dac35b3d5b7d267f57dfb51e475918ac08fb
EduardoDuran <Eduardo.duran@tradepoint.com.ec>
2014-09-21 23:10:49 -0500

    intro y login

--- gen/tesis/invenapp/R.java
+++ gen/tesis/invenapp/R.java
@@ -16,14 +16,27 @@ public final class R {
         public static final int logoinvenapp=0x7f020002;
     }
     public static final class id {
+        public static final int contrasena=0x7f060003;
         public static final int imageView1=0x7f060000;
+        public static final int login=0x7f060004;
+        public static final int registrarse=0x7f060005;
+        public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060006;
+        public static final int usuario=0x7f060001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int iniciarsesion=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int contrasena2=0x7f040007;
+        public static final int copy=0x7f040005;
         public static final int hello_world=0x7f040001;
+        public static final int iniciar=0x7f040002;
+        public static final int log1=0x7f040003;
+        public static final int registrarse=0x7f040004;
+        public static final int usuario2=0x7f040006;
     }
     public static final class style {
         /** 
